5ca29856fbfd38e1902e4661b9592d3868a49026
JyX <xavier.poindessault@gmail.com>
2025-03-04 12:53:49 +0100

    fix: comment out CORS origin configuration in backend config

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,11 +2,11 @@ const config = {
   port: process.env.PORT || 5000,
   dbPath: process.env.NODE_ENV === 'production' ? '/app/database.db' : './database.db',
   jwtSecret: process.env.JWT_SECRET || 'your_jwt_secret_default',
-  corsOrigin: (
-    process.env.NODE_ENV === 'production' 
-      ? ['http://srv743489.hstgr.cloud', 'http://is80s4w8kkccgko8808ookww.82.29.170.182.sslip.io'] // Update this with your frontend URL
-      : 'http://localhost:5173'
-  ),
+  // corsOrigin: (
+  //   process.env.NODE_ENV === 'production' 
+  //     ? ['http://srv743489.hstgr.cloud', 'http://is80s4w8kkccgko8808ookww.82.29.170.182.sslip.io'] // Update this with your frontend URL
+  //     : 'http://localhost:5173'
+  // ),
   socketMethods: ['GET', 'POST'],
   
   // Logging configuration
